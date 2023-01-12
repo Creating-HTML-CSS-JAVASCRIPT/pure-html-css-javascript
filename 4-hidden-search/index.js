6681cdfb41de043fee636049462ae4ff4d57c7de
VISH01AL <g.vish1al@gmail.com>
2023-01-12 20:38:42 +0530

    added 3 and modified 1

---
+++ 4-hidden-search/index.js
@@ -0,0 +1,11 @@
+const input=document.querySelectorAll('.input');
+
+function showinput(){
+    if(input[0].className==='input'){
+        input[0].className='inputnew'
+
+    }
+    else{
+        input[0].className='input'
+    }
+}
