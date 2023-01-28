5fcb982536031ffaf0658739a3b3014bf9e53f01
G Vishal <42919948+VISH01AL@users.noreply.github.com>
2023-01-28 21:41:34 +0530

    Update index.js
    removed the api key

--- 10-dad-jokes/index.js
+++ 10-dad-jokes/index.js
@@ -1,7 +1,7 @@
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '60f70f6bdbmshd85a1642c18fe80p103e88jsnbe044b36628a',
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'daddyjokes.p.rapidapi.com'
 	}
 };
