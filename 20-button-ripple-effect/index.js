eb84d8a3eb422e2c674e26d9c68989c72ca06805
VISH01AL <g.vish1al@gmail.com>
2023-02-14 20:45:44 +0530

    completed 20th project

---
+++ 20-button-ripple-effect/index.js
@@ -0,0 +1,15 @@
+const ripplebutton=document.getElementsByClassName('ripple-button')[0];
+const growingcircle=document.getElementsByClassName('temp')[0];
+ripplebutton.addEventListener('click',addgrowingcircle);
+function addgrowingcircle(event){
+    let x=event.offsetX;
+    let y=event.offsetY;
+
+    growingcircle.style.top=""+y+"px";
+    growingcircle.style.left=""+x+"px";
+    growingcircle.classList.add('growing-circle')
+    setTimeout(removeclass,310)
+}
+function removeclass(){
+growingcircle.classList.remove('growing-circle')
+}
